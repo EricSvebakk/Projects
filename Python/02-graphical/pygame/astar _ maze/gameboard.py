d8c2013e7d7e9537635570e86515686a2f06210c
Tepohe <ericatsvebakk@gmail.com>
2021-11-18 15:11:10 +0100

    Updated A* Pathfinder
    Program updated to support changes:
    -Added spacebar to start/stop program
    -Added R(reset) to randomize walls
    -Added C(lear) to remove all walls
    -Added P(oint) which picks random start- and end-points
    -Added I(nformation) which outputs grid-info
    -Added Left mouse-click which updates selected cells to walls
    -Added Right mouse-click which outputs selected cell-info

--- Python/02-graphical/pygame/astar _ maze/gameboard.py
+++ Python/02-graphical/pygame/astar _ maze/gameboard.py
@@ -4,223 +4,342 @@ import math
 from random import randint
 
 class Gameboard:
-    
-    def __init__(self, pygame, size, start, end, wallChance):
-        
-        self.pygame = pygame
-        
-        self.size = size
-        (self.width, self.height) = pygame.display.get_surface().get_size()
+	
+	def __init__(self, pygame, size, start, end, wallChance):
+		
+		self.pygame = pygame
+		
+		self.size = size
+		(self.width, self.height) = pygame.display.get_surface().get_size()
 
-        # 
-        self.cWidth = self.width / size
-        self.cHeight = self.height / size
-        
-        # 
-        self.wallChance = wallChance
-        
-        # 
-        self.startCell = None
-        self.endCell = None
-        self.current = None
-        
-        # 
-        self.backgroundCellColour = (255, 255, 255)
-        self.pathCellColour = (0, 0, 0)
-        self.startCellColour = (0, 255, 0)
-        self.closedCellColour = (255, 0, 0)
-        self.openCellColour = (100, 200, 100)
-        self.wallCellColour = (100, 100, 100)
+		# 
+		self.cWidth = self.width / size
+		self.cHeight = self.height / size
+		
+		# 
+		self.wallChance = wallChance
+		
+		# 
+		self.startCell = None
+		self.endCell = None
+		self.current = None
+		
+		# 
+		self.backgroundCellColour = (50, 50, 50)
+		self.wallCellColour = (0, 0, 0)
+		
+		self.startColour = (255, 0, 255)
+		self.endColour = (255, 255, 255)
+		self.pathColour = (0, 80, 0)
+		
+		self.closedSetColour = (255, 0, 0)
+		self.openSetColour = (100, 200, 100)
 
-        # 
-        self.closedSet = []
-        self.openSet = []
-        
-        # 
-        self.path = []
-        self.oldPath = []
+		self.updateFrequency = 100
+		self.pauseFrequency = 80
+		self.pygame.FPS = self.pauseFrequency
+		self.allowUpdate = True
+		self._mouseMode = 1
 
-        # 
-        self.grid = []
-        self.generate(start, end)
+		# 
+		self.closedSet = []
+		self.openSet = []
+		
+		# 
+		self.path = []
+		self.oldPath = []
 
-    #=============================================================================
-    def generate(self, start, end):
-        
-        self.startPos = start
-        self.endPos = end
+		# 
+		self.grid = []
+		self.createGrid()
+		self.randomizeCells()
+		self.pickPoints(start, end)
 
-        self.openSet = []
-        self.closedSet = []
-        self.path = []
-        self.oldPath = []
-        self.grid = []
+	#=============================================================================
+	def resetData(self):
+		
+		if self.startCell:
+			self.startCell.wall = False
+		
+		if self.endCell:
+			self.endCell.wall = False
+		
+		self.current = None
+		
+		self.openSet = []
+		self.closedSet = []
+		self.path = []
+		self.oldPath = []
+		self.openSet.append(self.startCell)
+		
+	
+	def createGrid(self):
+		
+		# Cell generation code
+		for y in range(self.size):
+			self.grid.append([])
+			for x in range(self.size):
+				cell = Cell(int(x), int(y), self.cWidth, self.cHeight)
+				self.grid[y].append(cell)
 
-        # Cell generation code
-        for y in range(self.size):
-            self.grid.append([])
-            for x in range(self.size):
-                            
-                cell = Cell(int(x), int(y), self.cWidth, self.cHeight)
-
-                if randint(0, int(100/self.wallChance)-1) == 0:
-                    cell.wall = True
-                else:
-                    cell.wall = False
-
-                self.grid[y].append(cell)
-
-        # Update each cell with their corresponding neighbours
-        for y in range(self.size):
-            for x in range(self.size):
-                
-                cell = self.grid[y][x]
-                cell.findNeighbours(self.grid, self.size)
-
-        # # Wall generation code
-        # for y in range(self.size):
-        #     for x in range(self.size):
-
-        #         cell = self.grid[y][x]
-                
-        #         if cell.x - 1 > 0 and cell.x < self.size-1:
-        #             if cell.y - 1 > 0 and cell.y < self.size-1:
-
-        #                 if self.grid[cell.y-1][cell.x].wall:
-        #                     if randint(0, 1) == 0:
-        #                         cell.wall = True
-
-        #                 if self.grid[cell.y][cell.x-1].wall or self.grid[cell.y][cell.x+1].wall:
-        #                     cell.wall = False
-
-        # Sets self.start to the corresponding cell for the given grid-coordinates
-        self.startCell = self.grid[start[1]][start[0]]
-        self.startCell.wall = False
-        
-        # start cell is first cell to be checked in the open-set
-        self.openSet.append(self.startCell)
-
-        # Sets self.end to the corresponding cell for the given grid-coordinates
-        self.endCell = self.grid[end[1]][end[0]]
-        self.endCell.wall = False
-
-    #=============================================================================
-    def update(self):
-
-        if self.current is self.endCell:
-            return
-
-        if len(self.openSet) > 0:
-
-            index = 0
-            for i in range(len(self.openSet)):
-                if self.openSet[i].f < self.openSet[index].f:
-                    index = i
-
-            self.current = self.openSet[index]
-            self.openSet.pop(index)
-            self.closedSet.append(self.current)
-
-            currentNeighbours = self.current.neighbours
-
-            for i in range(len(currentNeighbours)):
-                cell = currentNeighbours[i]
-                
-                if cell not in self.closedSet and not cell.wall:
-                    tempG = self.current.g + self.heuristic(cell, self.current)
-
-                    newPath = False
-                    if cell in self.openSet:
-                        if tempG < cell.g:
-                            cell.g = tempG
-                            newPath = True
-                    else:
-                        cell.g = tempG
-                        newPath = True
-                        self.openSet.append(cell)
-
-                    if newPath:
-                        cell.h = self.heuristic(cell, self.endCell)
-                        cell.f = cell.g + cell.h
-                        cell.previous = self.current
-
-        else:
-            print("no solution")
-            return
-
-        self.path = []
-        temp = self.current
-        self.path.append(temp)
-        if temp != None:
-            while temp.previous:
-                self.path.append(temp.previous)
-                temp = temp.previous
+		# Update each cell with their corresponding neighbours
+		for y in range(self.size):
+			for x in range(self.size):
+				
+				cell = self.grid[y][x]
+				cell.findNeighbours(self.grid, self.size)
+				
+		self.resetData()
 
 
-    #=============================================================================
-    def drawAll(self, screen):
+	def randomizeCells(self):
+		
+		for y in range(self.size):
+			for x in range(self.size):
+				
+				cell = self.grid[y][x]
+				
+				if randint(0, int(100/self.wallChance)-1) == 0:
+					cell.wall = True
+				else:
+					cell.wall = False
+		
+		# # Wall generation code
+		# for y in range(self.size):
+		#     for x in range(self.size):
 
-        colour = None
+		#         cell = self.grid[y][x]
 
-        for y in range(self.size):
-            for x in range(self.size):
+		#         if cell.x - 1 > 0 and cell.x < self.size-1:
+		#             if cell.y - 1 > 0 and cell.y < self.size-1:
 
-                cell = self.grid[y][x]
+		#                 if self.grid[cell.y-1][cell.x].wall:
+		#                     if randint(0, 1) == 0:
+		#                         cell.wall = True
 
-                if cell.wall:
-                    colour = self.wallCellColour
-                else:
-                    colour = self.backgroundCellColour
+		#                 if self.grid[cell.y][cell.x-1].wall or self.grid[cell.y][cell.x+1].wall:
+		#                     cell.wall = False
+		
+		self.resetData()
+	
+		
+	def clearBoard(self):
 
-                self.grid[y][x].draw(self.pygame, screen, colour)
+		for y in range(self.size):
+			for x in range(self.size):
+				self.grid[y][x].wall = False
 
-    #=============================================================================
-    def draw(self, screen):
+		self.resetData()
+	
+	
+	def pickPoints(self, start, end):
+				
+		# Sets self.start to the corresponding cell for the given grid-coordinates
+		self.startCell = self.grid[start[1]][start[0]]
 
-        colour = None
+		# # start cell is first cell to be checked in the open-set
+		# self.openSet.append(self.startCell)
 
-        for y in range(self.size):            
-            for x in range(self.size):
-                
-                cell = self.grid[y][x]
+		# Sets self.end to the corresponding cell for the given grid-coordinates
+		self.endCell = self.grid[end[1]][end[0]]
 
-                if cell is self.startCell or cell is self.endCell:
-                    colour = self.startCellColour
+		self.resetData()
 
-                elif cell in self.path:
-                    colour = self.pathCellColour
+	#=============================================================================
+	def update(self):
 
-                elif cell in self.oldPath:
-                    colour = self.closedCellColour
+		if self.current is self.endCell:
+			return
 
-                # elif cell in self.closedSet:
-                #     colour = (255, 0, 0)
+		if len(self.openSet) > 0:
 
-                elif cell in self.openSet:
-                    colour = self.openCellColour
+			index = 0
+			for i in range(len(self.openSet)):
+				if self.openSet[i].f < self.openSet[index].f:
+					index = i
 
-                else:
-                    continue
+			self.current = self.openSet[index]
+			self.openSet.pop(index)
+			self.closedSet.append(self.current)
 
-                self.grid[y][x].draw(self.pygame, screen, colour)
+			currentNeighbours = self.current.neighbours
 
-        self.oldColour = colour
+			for i in range(len(currentNeighbours)):
+				cell = currentNeighbours[i]
+				
+				if cell not in self.closedSet and not cell.wall:
+					tempG = self.current.g + self.heuristic(cell, self.current)
 
-        # coordinates = []
-        # for i in self.path:
-        #     center = (int(i.x*i.width+i.width/2), int(i.y*i.height+i.height/2))
-        #     coordinates.append(center)
-        
-        # if len(coordinates) > 1:
-        #     self.pygame.draw.lines(screen, (0,0,0), False, coordinates, 5)
+					newPath = False
+					if cell in self.openSet:
+						if tempG < cell.g:
+							cell.g = tempG
+							newPath = True
+					else:
+						cell.g = tempG
+						newPath = True
+						self.openSet.append(cell)
 
-        self.oldPath = self.path
+					if newPath:
+						cell.h = self.heuristic(cell, self.endCell)
+						cell.f = cell.g + cell.h
+						cell.previous = self.current
 
-    #=============================================================================
-    def heuristic(self, objA, objB):
+		else:
+			print("no solution")
+			return
 
-        # Manhattan distance. x distance + y distance
-        # return abs(objA.x - objB.x) + abs(objA.y - objB.y)
+		self.path = []
+		temp = self.current
+		self.path.append(temp)
+		if temp != None:
+			while temp.previous:
+				self.path.append(temp.previous)
+				temp = temp.previous
 
-        # euclidean distance. pythagoras.
-        return math.hypot(objA.x - objB.x, objA.y - objB.y)
+
+	#=============================================================================
+	def drawGrid(self, screen):
+
+		colour = None
+		
+		for y in range(self.size):
+			for x in range(self.size):
+
+				cell = self.grid[y][x]
+
+				if cell.wall:
+					colour = self.wallCellColour
+				else:
+					colour = self.backgroundCellColour
+					# continue
+
+				self.grid[y][x].draw(self.pygame, screen, colour)
+
+	#=============================================================================
+	def draw(self, screen):
+
+		colour = None
+
+		for y in range(self.size):            
+			for x in range(self.size):
+				
+				cell = self.grid[y][x]
+
+				if cell is self.startCell:
+					colour = self.startColour
+					
+				elif cell is self.endCell:
+					colour = self.endColour
+
+				elif cell in self.path:
+					colour = self.pathColour
+
+				elif cell in self.oldPath:
+					colour = self.closedSetColour
+
+				elif cell in self.openSet:
+					colour = self.openSetColour
+
+				# elif cell in self.closedSet:
+				#     colour = self.closedSetColour
+
+				else:
+					continue
+
+				self.grid[y][x].draw(self.pygame, screen, colour)
+
+		# self.oldColour = colour
+
+		# coordinates = []
+		# for i in self.path:
+		#     center = (int(i.x*i.width+i.width/2), int(i.y*i.height+i.height/2))
+		#     coordinates.append(center)
+		
+		# if len(coordinates) > 1:
+		#     self.pygame.draw.lines(screen, (0,0,0), False, coordinates, 5)
+
+		self.oldPath = self.path
+
+	#=============================================================================
+	def heuristic(self, objA, objB):
+
+		# Manhattan distance. x distance + y distance
+		# return abs(objA.x - objB.x) + abs(objA.y - objB.y)
+
+		# euclidean distance. pythagoras.
+		return math.hypot(objA.x - objB.x, objA.y - objB.y)
+	
+	def pause(self):
+		self.allowUpdate = True
+		self.pygame.FPS = self.pauseFrequency
+
+	def unpause(self):
+		self.pygame.FPS = self.updateFrequency
+		self.allowUpdate = False
+
+	def isPaused(self):
+		return self.allowUpdate
+		
+	def incrementFrequency(self, increment):
+		self.updateFrequency += increment
+		self.pygame.FPS = self.updateFrequency
+
+	def getFrequency(self):
+		return self.updateFrequency
+		
+	def setMouseMode(self, _mouseMode):
+		self._mouseMode = _mouseMode
+		
+	#
+	def selectWithMouse(self, mouseX, mouseY):
+
+		if not self.isPaused():
+			return
+
+		obj = None
+
+		cWidth = self.width / self.size
+		cHeight = self.height / self.size
+
+		# keyX = 0
+		# keyX = 0
+
+		for y in range(self.size):
+			for x in range(self.size):
+
+				x1 = x * cWidth
+				y1 = y * cHeight
+				x2 = x1 + cWidth
+				y2 = y1 + cHeight
+				obj = self.grid[y][x]
+
+				if mouseX >= x1 and mouseX < x2:
+					if mouseY >= y1 and mouseY < y2:
+						
+						
+						if self._mouseMode == 1:
+							obj.wall = True
+							
+							# ADD 'PAUSE WHEN MOUSEDOWN'
+							# ADD 'RESETDATA() IF DRAW'
+							# FIX COLOURING FOR BETTER PERFORMANCE
+							
+							
+							# obj.f = 0
+							# obj.g = 0
+							# obj.h = 0
+							
+							# self.path = []
+							# self.closedSet = []
+							# self.openSet = []
+							# self.openSet.append(self.startCell)
+								
+								
+						elif self._mouseMode == 3:
+							print(obj)
+							print("wall:      ", obj.wall)
+							print("path:      ", obj in self.path)
+							print("openSet:   ", obj in self.openSet)
+							print("closedSet: ", obj in self.closedSet)
+							# obj.wall = False
--- Python/02-graphical/pygame/astar _ maze/main.py
+++ Python/02-graphical/pygame/astar _ maze/main.py
@@ -1,14 +1,15 @@
 
 from gameboard import Gameboard
-import pygame
-import time
 from random import randint as ri
+import pygame
 
 
-backgroundColour = (255,255,255)
+backgroundColour = (50, 50, 50)
 
 screen = pygame.display.set_mode((600,600))
 pygame.display.set_caption("A* Search Algorithm")
+
+timerObject = pygame.time.Clock()
 
 size = 100
 start = [ri(0, size-1), ri(0, size-1)]
@@ -16,32 +17,99 @@ end = [ri(0, size-1), ri(0, size-1)]
 gameboard = Gameboard(pygame, size, start, end, 30)
 
 screen.fill(backgroundColour)
-gameboard.drawAll(screen)
+gameboard.drawGrid(screen)
 pygame.display.flip()
+
+dragging = False
 
 #=============================================================================
 # Gameloop variable
 running = True
 while running:
 
-    for event in pygame.event.get():
+	for event in pygame.event.get():
 
-        if event.type == pygame.QUIT:
-            running = False
+		if event.type == pygame.QUIT:
+			running = False
+			
+		elif event.type == pygame.MOUSEBUTTONDOWN:
+			if event.button == 1:
+				dragging = True
+				(xPos, yPos) = pygame.mouse.get_pos()
+				gameboard.setMouseMode(1)
+				gameboard.selectWithMouse(xPos, yPos)
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_q:
-                running = False
+			elif event.button == 3:
+				dragging = True
+				(xPos, yPos) = pygame.mouse.get_pos()
+				gameboard.setMouseMode(3)
+				gameboard.selectWithMouse(xPos, yPos)
 
-            elif event.key == pygame.K_r:
-                start = [ri(0, size-1), ri(0, size-1)]
-                end = [ri(0, size-1), ri(0, size-1)]
-                gameboard.generate(start, end)
-                screen.fill(backgroundColour)
-                gameboard.drawAll(screen)
+		elif event.type == pygame.MOUSEMOTION:
+			if dragging:
+				(xPos, yPos) = pygame.mouse.get_pos()
+				gameboard.selectWithMouse(xPos, yPos)
+				
 
-    # screen.fill(backgroundColour)
-    gameboard.update()
-    # gameboard.drawAll(screen)
-    gameboard.draw(screen)
-    pygame.display.flip()
+		elif event.type == pygame.MOUSEBUTTONUP:
+			dragging = False
+
+		elif event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_q:
+				running = False
+			
+			elif event.key == pygame.K_c:
+				gameboard.clearBoard()
+				gameboard.drawGrid(screen)
+				print("Board cleared")
+
+			elif event.key == pygame.K_r:
+				gameboard.randomizeCells()
+				gameboard.drawGrid(screen)
+				# gameboard.createGrid(start, end)
+				# screen.fill(backgroundColour)
+				# gameboard.drawAll(screen)
+			
+			elif event.key == pygame.K_p:
+				start = [ri(0, size-1), ri(0, size-1)]
+				end = [ri(0, size-1), ri(0, size-1)]
+				gameboard.pickPoints(start,end)
+				gameboard.drawGrid(screen)
+				
+				
+			elif event.key == pygame.K_i:
+				print("path:      ", gameboard.path)
+				print("openSet:   ", gameboard.openSet)
+				print("closedSet: ", gameboard.closedSet)
+			
+			elif event.key == pygame.K_SPACE:
+				if gameboard.isPaused():
+					gameboard.unpause()
+					print(f"board running ({pygame.FPS})")
+				else:
+					gameboard.pause()
+					print(f"board stopped ({pygame.FPS})")
+			
+			elif event.key == pygame.K_UP:
+				gameboard.incrementFrequency(1)
+				print(f"speed increased (FPS:{gameboard.getFrequency()})")
+
+			elif event.key == pygame.K_DOWN:
+				if gameboard.getFrequency() <= 1:
+					print(f"speed not decreased (FPS:{gameboard.getFrequency()})")
+				else:
+					gameboard.incrementFrequency(-1)
+					print(f"speed decreased (FPS:{gameboard.getFrequency()})")
+
+		# screen.fill(backgroundColour)
+		gameboard.drawGrid(screen)	
+		gameboard.draw(screen)
+		pygame.display.flip()
+	
+	if not gameboard.isPaused():
+		# screen.fill(backgroundColour)
+		gameboard.update()
+		gameboard.draw(screen)
+		pygame.display.flip()
+	
+	timerObject.tick(pygame.FPS)
