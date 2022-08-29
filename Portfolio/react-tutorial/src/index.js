829aa955118ca1a4d22655f3f8ad427de2864d2e
Tepohe <ericatsvebakk@gmail.com>
2022-08-29 13:37:15 +0200

    .gitignore updated

--- Portfolio/react-tutorial/src/index.js
+++ Portfolio/react-tutorial/src/index.js
@@ -1,33 +1,13 @@
 
 import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import ReactDOM from 'react-dom/client'
+import App from './components/App'
 import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
 
-// const heading1 = React.createElement(
-// 	"h1",
-// 	{className: "site-heading"},
-// 	"Hello, bud!"
-// )
-
-// const name = "Tania"
-// const heading2 = <h1>Hello, {name}</h1>
-	
-	
-	
-	
-// import reportWebVitals from './reportWebVitals';
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-		//   <React.StrictMode>
-		//     <App />
-//   </React.StrictMode>
-// );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+// 
+setInterval(
+	() => {root.render(<App />)}
+, 500);
