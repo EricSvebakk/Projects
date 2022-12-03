943e31eb323392ff1be9e87ceda2fc30a43010f7
Tepohe <ericatsvebakk@gmail.com>
2022-12-03 15:07:56 +0100

    Updates

--- precache-manifest.d4a03ef9046009fa1e7b0d1a57fb3463.js
+++ precache-manifest.d4a03ef9046009fa1e7b0d1a57fb3463.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = [
   {
-    "revision": "5047c3fc0e874997bf00",
-    "url": "/Projects/static/css/main.2e8ce219.chunk.css"
+    "revision": "1aabeaf68ce6915dc811",
+    "url": "/Projects/static/css/main.616476a3.chunk.css"
   },
   {
-    "revision": "5047c3fc0e874997bf00",
-    "url": "/Projects/static/js/main.5047c3fc.chunk.js"
+    "revision": "1aabeaf68ce6915dc811",
+    "url": "/Projects/static/js/main.1aabeaf6.chunk.js"
   },
   {
-    "revision": "9d80cd5aedf2e1e7fffb",
+    "revision": "90f6e13605a83f923ac8",
     "url": "/Projects/static/css/1.5af3c6e6.chunk.css"
   },
   {
-    "revision": "9d80cd5aedf2e1e7fffb",
-    "url": "/Projects/static/js/1.9d80cd5a.chunk.js"
+    "revision": "90f6e13605a83f923ac8",
+    "url": "/Projects/static/js/1.90f6e136.chunk.js"
   },
   {
     "revision": "47d60afdb927a5329998",
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/Projects/static/media/BW_UiO_logo.e422401a.png"
   },
   {
-    "revision": "995f4c7044edd9f590af0e8e8fd8dd42",
+    "revision": "9b4b634ae5ba67c2c33ea2fcb11367f6",
     "url": "/Projects/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Projects/precache-manifest.78f2c0fce8d0fcf75cd3d3b4a8f40ee0.js"
+  "/Projects/precache-manifest.d4a03ef9046009fa1e7b0d1a57fb3463.js"
 );
 
 workbox.clientsClaim();
