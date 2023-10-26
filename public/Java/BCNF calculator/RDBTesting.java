79529b528ad6ad72c36fa5b7f99f99ed0b07fd5c
Tepohe <ericatsvebakk@gmail.com>
2023-10-26 03:07:24 +0200

    Added tests for BCNF-calculator

--- public/Java/BCNF calculator/RDBTesting.java
+++ public/Java/BCNF calculator/RDBTesting.java
@@ -1,12 +1,18 @@
 import java.io.File;
+import java.util.Arrays;
+import java.util.HashSet;
+
+// import testing.BCNF2;
 
 public class RDBTesting {
 	
 	public static void main(String[] args) {
 		
-		RDB r = new RDB(new File("testing/h22eksamen.txt"));
+		RDB r = new RDB(new File("testing/julie_data.txt"));
 		
 		System.out.println(r);
+		
+		
 		
 		// for (String ck : r.RDB_CKs) {
 		// 	System.out.println(ck + " " + r.getClosure(ck, r.RDB_FDs));
@@ -16,6 +22,32 @@ public class RDBTesting {
 		// FDs.put("BCF", "D");
 		// System.out.println("**** " + r.findNFs(FDs, r.RDB_CKs));
 		
-		r.decompose("A");
+		// r.decompose("A");
+		
+		
+		System.out.println("CLOSURE");
+		
+		String l = "NKA";
+		// String combos = r.
+		String result = r.getClosure(l, r.RDB_FDs);
+		
+		System.out.println("FDs: " + r.RDB_FDs);
+		System.out.println("result ("+ l +"): " + result);
+		
+		
+		
+		HashSet<String> stuff = new HashSet<>();
+		
+		stuff.add("a");
+		stuff.add("b");
+		stuff.add("c");
+		
+		String res = String.join("", stuff);
+		
+		System.out.println(res);
+		
+		HashSet<String> s = new HashSet<>(Arrays.asList(res.split("")));
+		
+		System.out.println("result: " + s);
 	}
 }
